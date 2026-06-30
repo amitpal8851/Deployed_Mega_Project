ce0b6237ef6fd64a4c706c15025fe531058808a5
Amit Pal <amitpalmi0@gmail.com>
2026-06-30 16:53:36 +0530

    fix: correct typo in appwriteBucketId variable name

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -3,7 +3,7 @@ const conf = {
     appwriteProjectId : String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
     appwriteDatabaseId : String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     appwriteCollectionId : String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-    appwriteBucketId : String(import.meta.env.VITE_APPWRITE_BUCKETID_ID),
+    appwriteBucketId : String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
     tinyMceKey : String(import.meta.env.VITE_TINYMCE_API_KEY)
 };
 
